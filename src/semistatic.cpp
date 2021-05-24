8a61b049efcb3ca430567ea506c2eaf58fe0e0db
ambrona <mac.ambrona@gmail.com>
2021-05-25 02:51:04 +0900

    Old syntax for en passant

--- src/semistatic.cpp
+++ src/semistatic.cpp
@@ -333,7 +333,7 @@ bool SemiStatic::is_unwinnable(Position& pos, Color intendedWinner, int trivialP
 
   // If en passant is possible, return false
   for (const auto& m : MoveList<LEGAL>(pos))
-    if (type_of(m) == EN_PASSANT)
+    if (type_of(m) == ENPASSANT)
       return false;
 
   // Trivial progress: as long as there is only one legal move, make that move
